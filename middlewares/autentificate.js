1f999b43e3c09c4b23d69b73746fbbb7a9e7290b
IrynaYermolova <ermolova_ira@ukr.net>
2023-07-28 13:25:38 +0300

    done

--- middlewares/autentificate.js
+++ middlewares/autentificate.js
@@ -27,28 +27,3 @@ const authenticate = async (req, res, next) => {
 
 module.exports = authenticate;
 
-// const jwt = require('jsonwebtoken');
-// const { User } = require('../models/user');
-// const { HttpError } = require('../helpers/HttpError');
-// const { SECRET_KEY } = process.env;
-
-// const autenticate = async (req, res, next) => {
-//     const { authorization = "" } = req.headers;
-//     const [bearer, token] = authorization.split(" ");
-//     if (bearer !== "Bearer") {
-//       next(HttpError(401));
-//     }
-//     try {
-//       const { id } = jwt.verify(token, SECRET_KEY);
-//       const user = await User.findById(id);
-//       if (!user) {
-//         next(HttpError(401));
-//       }
-//       req.user = user;
-//       next();
-//     } catch (error) {
-//       next(HttpError(401));
-//     }
-//   };
-  
-//   module.exports = autenticate;
